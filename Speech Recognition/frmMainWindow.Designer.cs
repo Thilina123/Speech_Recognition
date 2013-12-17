ed8d946402fbbebd9026e77da613dc8093cd3a4d
Thilina123 <teez.mrt@gmail.com>
2013-12-17 15:35:20 +0530

    Maps Integrated
    initial voice recognition

---
+++ Speech Recognition/frmMainWindow.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Speech_Recognition
+{
+    partial class frmMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.button1btnSearch = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.zoomLevels = new System.Windows.Forms.ComboBox();
+            this.mapTypeCombo = new System.Windows.Forms.ComboBox();
+            this.fullAddress = new System.Windows.Forms.TextBox();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.mapViewer = new System.Windows.Forms.WebBrowser();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1btnSearch);
+            this.groupBox1.Controls.Add(this.txtLocation);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(859, 42);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Location";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(9, 16);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(763, 20);
+            this.txtLocation.TabIndex = 0;
+            this.txtLocation.TextChanged += new System.EventHandler(this.txtLocation_TextChanged);
+            // 
+            // button1btnSearch
+            // 
+            this.button1btnSearch.Location = new System.Drawing.Point(778, 12);
+            this.button1btnSearch.Name = "button1btnSearch";
+            this.button1btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.button1btnSearch.TabIndex = 1;
+            this.button1btnSearch.Text = "Search";
+            this.button1btnSearch.UseVisualStyleBackColor = true;
+            this.button1btnSearch.Click += new System.EventHandler(this.button1btnSearch_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mapViewer);
+            this.panel1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.panel1.Location = new System.Drawing.Point(175, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(689, 519);
+            this.panel1.TabIndex = 1;
+            // 
+            // zoomLevels
+            // 
+            this.zoomLevels.FormattingEnabled = true;
+            this.zoomLevels.Items.AddRange(new object[] {
+            "5",
+            "8",
+            "10",
+            "12",
+            "15",
+            "18",
+            "20"});
+            this.zoomLevels.Location = new System.Drawing.Point(14, 65);
+            this.zoomLevels.Name = "zoomLevels";
+            this.zoomLevels.Size = new System.Drawing.Size(155, 21);
+            this.zoomLevels.TabIndex = 2;
+            this.zoomLevels.Text = "Select Zoom Level";
+            this.zoomLevels.SelectedIndexChanged += new System.EventHandler(this.zoomLevels_SelectedIndexChanged);
+            // 
+            // mapTypeCombo
+            // 
+            this.mapTypeCombo.FormattingEnabled = true;
+            this.mapTypeCombo.Items.AddRange(new object[] {
+            "roadmap",
+            "satellite",
+            "hybrid",
+            "terrain"});
+            this.mapTypeCombo.Location = new System.Drawing.Point(14, 92);
+            this.mapTypeCombo.Name = "mapTypeCombo";
+            this.mapTypeCombo.Size = new System.Drawing.Size(155, 21);
+            this.mapTypeCombo.TabIndex = 3;
+            // 
+            // fullAddress
+            // 
+            this.fullAddress.Location = new System.Drawing.Point(14, 119);
+            this.fullAddress.Multiline = true;
+            this.fullAddress.Name = "fullAddress";
+            this.fullAddress.ReadOnly = true;
+            this.fullAddress.Size = new System.Drawing.Size(155, 191);
+            this.fullAddress.TabIndex = 4;
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.Location = new System.Drawing.Point(14, 316);
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.Size = new System.Drawing.Size(155, 20);
+            this.txtLongitude.TabIndex = 5;
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.Location = new System.Drawing.Point(14, 342);
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.Size = new System.Drawing.Size(155, 20);
+            this.txtLatitude.TabIndex = 6;
+            // 
+            // mapViewer
+            // 
+            this.mapViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapViewer.Location = new System.Drawing.Point(0, 0);
+            this.mapViewer.MinimumSize = new System.Drawing.Size(20, 20);
+            this.mapViewer.Name = "mapViewer";
+            this.mapViewer.Size = new System.Drawing.Size(689, 519);
+            this.mapViewer.TabIndex = 0;
+            // 
+            // frmMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 562);
+            this.Controls.Add(this.txtLatitude);
+            this.Controls.Add(this.txtLongitude);
+            this.Controls.Add(this.fullAddress);
+            this.Controls.Add(this.mapTypeCombo);
+            this.Controls.Add(this.zoomLevels);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Location = new System.Drawing.Point(5, 4);
+            this.Name = "frmMainWindow";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1btnSearch;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox zoomLevels;
+        private System.Windows.Forms.ComboBox mapTypeCombo;
+        private System.Windows.Forms.TextBox fullAddress;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.TextBox txtLatitude;
+        private System.Windows.Forms.WebBrowser mapViewer;
+    }
+}
---
+++ Speech Recognition/frmMainWindow.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using Google.Api.Maps.Service.Geocoding;
+using Google.Api.Maps.Service;
+using Google.Api.Maps.Service.StaticMaps;
+
+namespace Speech_Recognition
+{
+    public partial class frmMainWindow : Form
+    {
+        SpeechRecognizer sr;
+        int bnaCounter = 0;
+        private GeocodingResult currentSelectedLocation;
+        public frmMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Create a new SpeechRecognizer instance.
+            sr = new SpeechRecognizer();
+
+            // Create a simple grammar that recognizes "red", "green", or "blue".
+            Choices jargon_grammar = new Choices();
+            //jargon_grammar.Add(new string[] { "left", "right", "up" ,"down","galewela"});
+
+
+            // Create a GrammarBuilder object and append the Choices object.
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(jargon_grammar);
+
+            // Create the Grammar instance and load it into the speech recognizer.
+            Grammar g = new Grammar(gb);
+            //sr.LoadGrammar(g);
+            Console.WriteLine("==================================");
+            // Register a handler for the SpeechRecognized event.
+            sr.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized);
+        }
+
+        // Create a simple handler for the SpeechRecognized event.
+        void sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            MessageBox.Show(e.Result.Text );
+            var request = new GeocodingRequest();
+            request.Address = e.Result.Text;
+            request.Sensor = "false";
+            var response = GeocodingService.GetResponse(request);
+            if (response.Status == ServiceResponseStatus.Ok)
+            {
+                currentSelectedLocation = response.Results.First();
+            }
+            updateMap();
+            //================
+            //var result = (GeocodingResult)currentSelectedLocation;
+            //fullAddress.Text = result.FormattedAddress;
+            //var location = result.Geometry.Location;
+            //var map = new StaticMap();
+            //map.Center = location.Latitude.ToString() + "," + location.Longitude.ToString();
+            //txtLatitude.Text = location.Latitude.ToString();
+            //txtLongitude.Text = location.Longitude.ToString();
+            //map.Zoom =
+            //(zoomLevels.SelectedItem == null) ? "10" : zoomLevels.Text.ToString();
+            //map.Markers = map.Center;
+            //map.Size = "1000" + "x" + "485";
+            //map.MapType =
+            //(mapTypeCombo.SelectedItem == null) ? "roadmap" : mapTypeCombo.Text.ToString();
+            //map.Sensor = "false";
+            //String urlToMap = map.ToUri().AbsoluteUri.ToString();
+            //mapViewer.Navigate(urlToMap);
+
+        }
+
+        private void button1btnSearch_Click(object sender, EventArgs e)
+        {
+            var request = new GeocodingRequest();
+            request.Address = txtLocation.Text;
+            request.Sensor = "false";
+            var response = GeocodingService.GetResponse(request);
+            if (response.Status == ServiceResponseStatus.Ok)
+            {
+                currentSelectedLocation = response.Results.First();
+            }
+            updateMap();
+        }
+
+        private void updateMap()
+        {
+            var result = (GeocodingResult)currentSelectedLocation;
+            fullAddress.Text = result.FormattedAddress;
+            var location = result.Geometry.Location;
+            var map = new StaticMap();
+            map.Center = location.Latitude.ToString() + "," +location.Longitude.ToString();
+            txtLatitude.Text = location.Latitude.ToString();            
+            txtLongitude.Text = location.Longitude.ToString();
+            map.Zoom =
+            (zoomLevels.SelectedItem == null) ? "10" : zoomLevels.Text.ToString();
+            map.Markers = map.Center;
+            map.Size = "1000" + "x" + "485";
+            map.MapType =
+            (mapTypeCombo.SelectedItem == null) ? "roadmap" : mapTypeCombo.Text.ToString();
+            map.Sensor = "false";
+            String urlToMap = map.ToUri().AbsoluteUri.ToString();
+            mapViewer.Navigate(urlToMap);
+        }
+
+        private void zoomLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            updateMap();
+        }
+
+        private void txtLocation_TextChanged(object sender, EventArgs e)
+        {
+            String str = txtLocation.Text;
+            String[] input =str.Split(new char[' ']);
+            try
+            {
+                if (input[input.Length - 1] == "search")
+                {
+                    var request = new GeocodingRequest();
+                    request.Address = txtLocation.Text;
+                    request.Sensor = "false";
+                    var response = GeocodingService.GetResponse(request);
+                    if (response.Status == ServiceResponseStatus.Ok)
+                    {
+                        currentSelectedLocation = response.Results.First();
+                    }
+
+                    updateMap();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception caught");
+            }
+
+        }
+    }
+}
